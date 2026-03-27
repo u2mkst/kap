00d477a64e35044c65fb904b6d5fdf1e51f894f4
YH WINGS <yhwings21@gmail.com>
2026-03-27 23:59:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3546941391-41e73",
+  "appId": "1:1004026489636:web:881169c87252b19a4f21b1",
+  "apiKey": "AIzaSyB-OmLj2FRiHBvwoBCiSX0dZ5T73zrA3lU",
+  "authDomain": "studio-3546941391-41e73.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1004026489636"
+};
